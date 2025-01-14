d8a4d7fa1ff5b3a342fb9a76d621e4a991076642
Erwin <108355646+Erwin0925@users.noreply.github.com>
2025-01-14 13:01:34 +0800

    Initial commit

---
+++ src/test/java/io/github/erwin/taskify_api/TaskifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.erwin.taskify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
