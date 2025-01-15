ea8d0d18932b3503fed39b8af8252fadcbed02e4
Erwin <108355646+Erwin0925@users.noreply.github.com>
2025-01-15 16:29:20 +0800

    -

--- src/main/java/io/github/erwin/taskify_api/controller/UserInfoController.java
+++ src/main/java/io/github/erwin/taskify_api/controller/UserInfoController.java
@@ -1,5 +1,7 @@
 package io.github.erwin.taskify_api.controller;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import io.github.erwin.taskify_api.model.UserInfo;
 import io.github.erwin.taskify_api.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/user")
 public class UserInfoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserInfoController.class);
+
     private UserInfoService userInfoService;
 
     @Autowired
@@ -21,19 +25,27 @@ public class UserInfoController {
 
     @PutMapping("/update-info")
     public ResponseEntity<String> UpdateUserInfo (@RequestBody UserInfo userInfo, Authentication authentication){
+        logger.info("Received request to update user info for user : {}", authentication.getName());
+
         try{
             userInfoService.updateUserInfo(userInfo, authentication);
+            logger.info("User info for '{}' updated successfully.", authentication.getName());
             return ResponseEntity.ok("User info updated Successfully!");
         }catch (Exception e){
+            logger.error("Error updating user info for '{}' : ", authentication.getName());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update UserDetails");
         }
     }
     @DeleteMapping("/deleteUser")
     public ResponseEntity<String> deleteUser(@RequestParam String userName){
+        logger.info("Received request to delete user : '{}'", userName);
+
         try {
             userInfoService.deleteUser(userName);
+            logger.info("User '{}' deleted successfully", userName);
             return ResponseEntity.ok(userName + " got deleted successfully");
         }catch (Exception e){
+            logger.error("Error deleting user '{}' : {}",userName, e.getMessage(),e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete "+ userName);
         }
     }
--- src/main/java/io/github/erwin/taskify_api/service/UserInfoServiceImpl.java
+++ src/main/java/io/github/erwin/taskify_api/service/UserInfoServiceImpl.java
@@ -3,13 +3,19 @@ package io.github.erwin.taskify_api.service;
 import io.github.erwin.taskify_api.model.UserInfo;
 import io.github.erwin.taskify_api.model.UserLogin;
 import io.github.erwin.taskify_api.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 public class UserInfoServiceImpl implements UserInfoService{
+
+    private static final Logger logger = LoggerFactory.getLogger(UserInfoServiceImpl.class);
 
     private final UserRepository userRepository;
 
@@ -20,14 +26,23 @@ public class UserInfoServiceImpl implements UserInfoService{
 
     @Override
     public void updateUserInfo(UserInfo userInfo, Authentication authentication) {
+        long startTime = System.currentTimeMillis();
+
+        logger.info("Starting updateUserInfo for user : {}", authentication.getName());
+
         if (userInfo == null){
+            logger.error("UserInfo is null for user : {}", authentication.getName());
             throw new IllegalArgumentException("Username and UserInfo cannot be NULL");
         }
 
         String username = authentication.getName();
 
         UserLogin userLogin = userRepository.findByUserName(username)
-                .orElseThrow(()-> new UsernameNotFoundException("UserName not Found!!"));
+                .orElseThrow(()-> {
+                    logger.error("User not found : {}", username);
+                    return new UsernameNotFoundException("UserName not Found!!");
+                });
+
 
         UserInfo olduserInfo = userLogin.getUserDetails();
         olduserInfo.setFullName(userInfo.getFullName());
@@ -35,12 +50,23 @@ public class UserInfoServiceImpl implements UserInfoService{
         olduserInfo.setLocation(userInfo.getLocation());
 
         userRepository.save(userLogin);
+        logger.info("user info updated successfully for user : {}", username);
+
+        long endTime = System.currentTimeMillis();
+        logger.info("Execution time for updateUserInfo : {} ms", (endTime-startTime));
     }
 
     @Override
     public void deleteUser(String userName) {
+        logger.info("Starting deleteUser for username: {}", userName);
+
         UserLogin userLogin = userRepository.findByUserName(userName)
-                .orElseThrow(()-> new UsernameNotFoundException("UserName not Found!!"));
+                .orElseThrow(() -> {
+                    logger.error("User not found: {}", userName);
+                    return new UsernameNotFoundException("UserName not Found!!");
+                });
+
         userRepository.delete(userLogin);
+        logger.info("User deleted successfully: {}", userName);
     }
 }
